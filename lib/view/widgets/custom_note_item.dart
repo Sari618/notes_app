b92f10652c1a1a8ccab04d3cfa8dbdd782366f4c
Win -11 <sari.ahmed25@gmail.com>
2024-10-08 14:30:58 +0300

    created Custom Note v

--- lib/view/widgets/custom_note_item.dart
+++ lib/view/widgets/custom_note_item.dart
@@ -1,55 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/view/edit_note_view.dart';
 
 class NotesItem extends StatelessWidget {
   const NotesItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
-          borderRadius: BorderRadius.circular(16)),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16,bottom: 16),
-              child: Text(
-                'Build ur cearer with me ',
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context){
+          return  const EditNoteView();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+            color: const Color(0xffFFCC80),
+            borderRadius: BorderRadius.circular(16)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
                 style: TextStyle(
-                  color: Colors.black.withOpacity(0.5),
-                  fontFamily: "Kanit",
-                  fontSize: 18,
+                  color: Colors.black,
+                  fontSize: 26,
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16,bottom: 16),
+                child: Text(
+                  'Build ur cearer with me ',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(0.5),
+                    fontFamily: "Kanit",
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  size: 24,
+                  color: Colors.black,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.trash,
-                size: 24,
-                color: Colors.black,
+            const Padding(
+              padding: EdgeInsets.only(right: 24),
+              child: Text(
+                'oct3,2024',
+                style: TextStyle(fontSize: 24,color: Colors.black),
               ),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(right: 24),
-            child: Text(
-              'oct3,2024',
-              style: TextStyle(fontSize: 24,color: Colors.black),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
