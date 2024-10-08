ae05b627af843e43043316b23ba5894c5f11a82e
Win -11 <sari.ahmed25@gmail.com>
2024-10-08 13:27:09 +0300

    create constance file ,added textfields to buttonsheet

--- lib/view/widgets/add_note_buttonsheet.dart
+++ lib/view/widgets/add_note_buttonsheet.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/view/widgets/custom_textfield.dart';
 
 class AddNoteButtonSheet extends StatelessWidget {
   const AddNoteButtonSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(); 
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal:16),
+      child: const Column(
+        children: [
+          SizedBox(
+            height: 32,
+          ),
+          CustomTextField(hint: 'Title',
+
+          ),
+          SizedBox(height: 16,)
+,          CustomTextField(hint: 'Content',
+          maxLines: 5,
+          ),
+        ],
+      ),
+    ); 
   }
 }
---
+++ lib/view/widgets/custom_textfield.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constance.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key, required this.hint,  this.maxLines=1});
+  final String hint;
+  final int maxLines;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor:kPrimaryColor ,
+      decoration: InputDecoration(
+        hintText: hint,
+        hintMaxLines: maxLines,
+        hintStyle:const TextStyle(color: kPrimaryColor),
+        border: buildBorder(),
+        enabledBorder:buildBorder(),
+        focusedBorder: buildBorder(
+          Color(0xff62FCD7),
+        ),
+      ),
+      
+    );
+  }
+
+  OutlineInputBorder buildBorder([color]) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide:  BorderSide(
+        color:color?? Colors.white,
+      )
+    );
+  }
+}
